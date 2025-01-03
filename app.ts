35b7d7db791119188dea6835e195d367a3befee2
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-01-03 15:02:16 +0530

    feat: implement 404 and error page

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 import indexRouter from "./routes/indexRouter";
 import newRouter from "./routes/newRouter";
 
@@ -12,6 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404).render("404", { url: req.originalUrl });
+});
+
+// Error Handler
+const errorRequestHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.status || 500).render("error", {
+    message: err.message,
+  });
+};
+
+app.use(errorRequestHandler);
 
 const PORT = process.env.PORT ?? 3000;
 
