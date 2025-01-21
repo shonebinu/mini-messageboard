99a3c52d091b033bf97a7dc44882701a58fe452f
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-01-21 21:28:49 +0530

    chore: change environment variables to a single db url

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -3,8 +3,6 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const { DB_NAME, DB_ROLE, DB_PASS, DB_HOST, DB_PORT } = process.env;
-
 export default new Pool({
-  connectionString: `postgresql://${DB_ROLE}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  connectionString: process.env.DB_URL,
 });
