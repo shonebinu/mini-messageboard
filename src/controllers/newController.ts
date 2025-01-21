a18a9bc5f496ec3a1244b842bbbdee1d529e541f
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-01-21 20:32:24 +0530

    setup express validator

--- src/controllers/newController.ts
+++ src/controllers/newController.ts
@@ -1,6 +1,7 @@
 import asyncErrorHandler from "../utils/asyncErrorHandler.js";
 import type { RequestHandler } from "express";
 import db from "../db/queries.js";
+import { body, validationResult } from "express-validator";
 
 const getNewMessageForm: RequestHandler = asyncErrorHandler(
   async (req, res) => {
@@ -8,10 +9,33 @@ const getNewMessageForm: RequestHandler = asyncErrorHandler(
   }
 );
 
-const createMessage: RequestHandler = asyncErrorHandler(async (req, res) => {
-  const { authorName, authorMessage } = req.body;
-  db.insertMessage(authorName, authorMessage, new Date());
-  res.redirect("/");
-});
+const validateMessage = [
+  body("authorName")
+    .trim()
+    .notEmpty()
+    .withMessage("Name shouldn't be empty")
+    .isAlpha()
+    .withMessage("Name should only contain letters."),
+  body("authorMessage")
+    .trim()
+    .notEmpty()
+    .withMessage("Message shouldn't be empty"),
+];
+
+const createMessage = [
+  validateMessage,
+  asyncErrorHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("error", {
+        message:
+          "Both author name and message shouldn't be empty. Author name should only contain letters.", // errors.array()
+      });
+    }
+    const { authorName, authorMessage } = req.body;
+    db.insertMessage(authorName, authorMessage, new Date());
+    res.redirect("/");
+  }),
+];
 
 export { getNewMessageForm, createMessage };
